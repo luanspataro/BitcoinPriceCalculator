f7e93bfa5e2a343559851d48655cd8dfd67ddc8d
Luan Spataro <luan.spataro@hotmail.com>
2024-07-04 16:57:16 -0300

    Att

---
+++ BitcoinPriceCalculator/Models/PriceCalculatorModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BitcoinPriceCalculator.Models
+{
+    public class PriceCalculatorModel
+    {
+        public int Currency { get; set; }
+        public int ActualPrice { get; set; }
+        public DateTime PurchaseDate { get; set; }
+
+    }
+}
