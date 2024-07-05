37ebcc3526e2ac4099f6c3d97b6254366c9e2903
Luan Spataro <luan.spataro@hotmail.com>
2024-07-05 16:42:16 -0300

    Testando leitura de excel

---
+++ BitcoinPriceCalculator/Models/BitcoinService.cs
@@ -0,0 +1,41 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BitcoinPriceCalculator.Models
+{
+    public class BitcoinService
+    {
+        private static List<Bitcoin> ReadXls()
+        {
+            var response = new List<Bitcoin>();
+
+            FileInfo existingFile = new FileInfo(fileName:"\Models\BitcoinDatePriceBRL");
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            using (ExcelPackage package = new ExcelPackage(existingFile))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[PositionID: 0];
+                int colCount = worksheet.Dimension.End.Column;
+
+                int rowCount = worksheet.Dimension.End.Row;
+
+                for (int row = 2; row <= rowCount; row++)
+                {
+                    var bitcoin = new Bitcoin();
+                    bitcoin.Price = Convert.ToDecimal(worksheet.Cells[row, Col: 1].Value);
+                    bitcoin.PriceDate = Convert.ToDateTime(worksheet.Cells[row, Col: 2].Value);
+
+                    response.Add(bitcoin);
+                }
+            }
+
+            return response;
+
+        }
+    }
+}
--- BitcoinPriceCalculator/Models/PriceCalculatorModel.cs
+++ BitcoinPriceCalculator/Models/PriceCalculatorModel.cs
@@ -7,12 +7,19 @@ namespace BitcoinPriceCalculator.Models
 {
     public class PriceCalculatorModel
     {
-        private double BTCAmount { get; set; }
-        private double Profit { get; set; }
+        public double BTCAmount { get; set; }
+        public double Profit { get; set; }
 
         public string Currency { get; set; }
         public int ActualPrice { get; set; }
         public DateTime PurchaseDate { get; set; }
 
     }
+
+    public class Bitcoin
+    {
+        public decimal Price { get; set; }
+        public DateTime PriceDate { get; set; }
+    }
+
 }
