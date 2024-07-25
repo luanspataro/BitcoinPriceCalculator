cdad7196fb9d706cdcd78621a4d181f0d13d1f14
Luan Spataro <luan.spataro@hotmail.com>
2024-07-25 16:51:52 -0300

    Ajuste de cores e mudanças no design

--- BitcoinPriceCalculator/wwwroot/js/site.js
+++ BitcoinPriceCalculator/wwwroot/js/site.js
@@ -21,15 +21,15 @@
                                     var resultDiv = document.getElementById("result");
     if (data.success) {
         resultDiv.innerHTML = `<div class="mb-0">
-<h4 id="btc-title" class="mt-3">Preço do Bitcoin:</h4>
-    <p id="btc-title">
+<h4 class="mt-3">Preço do Bitcoin:</h4>
+    <p>
         ${data.price}
     </p>
 </div>
 <div class="d-flex justify-content-center gif">
 <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script>
 
-    <dotlottie-player src="https://lottie.host/fbb4a9d1-1d24-4070-9e6a-0aedc8dd7837/sNJww6gjga.json" background="transparent" speed="1" style="width: 300px; height: 300px;" loop autoplay></dotlottie-player>
+    <dotlottie-player src="https://lottie.host/fbb4a9d1-1d24-4070-9e6a-0aedc8dd7837/sNJww6gjga.json" background="transparent" speed="1" style="width: 200px; height: 230px;" loop autoplay></dotlottie-player>
 </div>`;
                                     } else {
         resultDiv.innerHTML = `<p class="text-danger">Erro: ${data.error}</p>`;
